b71cd901044145e24e4d20696e1710299401cc4e
Kens04 <kento090403@icloud.com>
2024-12-19 09:17:10 +0900

    スキーマ修正

---
+++ prisma/migrations/20241219000552_/migration.sql
@@ -0,0 +1,39 @@
+-- CreateEnum
+CREATE TYPE "TodoStatus" AS ENUM ('UNREAD', 'READING', 'COMPLETED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "avatarUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "todos" (
+    "id" UUID NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "text" TEXT,
+    "status" "TodoStatus" NOT NULL DEFAULT 'UNREAD',
+    "category" TEXT,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+    "isFavorite" BOOLEAN NOT NULL DEFAULT false,
+    "userId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "completedAt" TIMESTAMP(3),
+
+    CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "todos" ADD CONSTRAINT "todos_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
