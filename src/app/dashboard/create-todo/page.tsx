90265a9aaac6bc6c4a929aff22c7f7c75ec677c0
Kens04 <kento090403@icloud.com>
2024-12-25 09:22:46 +0900

    中央寄せに変更

--- src/app/dashboard/create-todo/page.tsx
+++ src/app/dashboard/create-todo/page.tsx
@@ -5,7 +5,7 @@ import CreateTodoForm from "@/components/todo/create-todo/create-todo-form";
 export default function CreateTodo() {
   return (
     <Container size="md" w="100%" mt="lg">
-      <Title order={2} mb="md">
+      <Title order={2} mb="md" ta="center">
         TODO作成
       </Title>
       <Container size="xs" w="100%" mt="lg" p={0}>
