292b6249844f00ad4396a8b899ba63aa19df88dd
Kens04 <kento090403@icloud.com>
2024-12-26 09:41:35 +0900

    ログアウト時CSSが当たらないバグ修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import { getSession } from "@/components/hooks/useSession";
 import TopHeader from "@/components/top/header";
 import MainSection from "@/components/top/main-section";
+import { MantineProvider } from "@mantine/core";
 
 export default async function Home() {
   const session = await getSession();
 
   return (
-    <>
+    <MantineProvider>
       <TopHeader session={session}>
         <MainSection />
       </TopHeader>
-    </>
+    </MantineProvider>
   );
 }
