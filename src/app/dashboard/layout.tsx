2d9d9f1b999a4967f3a7fc3b8f426a8deba75a74
Kens04 <kento090403@icloud.com>
2024-12-26 09:42:14 +0900

    ログイン時、CSSが当たらないバグ修正

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,8 @@
-import '@mantine/charts/styles.css';
+import "@mantine/charts/styles.css";
 import DashboardLayout from "@/components/layout/dashboard";
 import type { Metadata } from "next";
 import { getSession } from "@/components/hooks/useSession";
+import { MantineProvider } from "@mantine/core";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +18,9 @@ export default async function Layout({
   return (
     <html lang="ja">
       <body>
-        <DashboardLayout session={session}>{children}</DashboardLayout>
+        <MantineProvider>
+          <DashboardLayout session={session}>{children}</DashboardLayout>
+        </MantineProvider>
       </body>
     </html>
   );
