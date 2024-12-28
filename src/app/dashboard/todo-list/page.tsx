0fca805f953898fe4ccd2421307e2f9325d4e911
Kens04 <kento090403@icloud.com>
2024-12-29 08:58:22 +0900

    ページコメントアウト

--- src/app/dashboard/todo-list/page.tsx
+++ src/app/dashboard/todo-list/page.tsx
@@ -1,21 +1,21 @@
-// import { getSession } from "@/components/hooks/useSession";
-// import { Todos } from "@/components/todo/action";
-// import TodoListContent from "@/components/todo/todo-list-content";
-// import { TodoStatus, type TodoList } from "@/components/todo/type";
+import { getSession } from "@/components/hooks/useSession";
+import { Todos } from "@/components/todo/action";
+import TodoListContent from "@/components/todo/todo-list-content";
+import { TodoStatus, type TodoList } from "@/components/todo/type";
+import { Suspense } from "react";
 
 const TodoList = async () => {
-        // const todos = await Todos();
-        // const allTodos = todos || [];
-  // const activeTodos = allTodos.filter(
-  //   (todo: TodoList) => todo.status !== TodoStatus.COMPLETED
-  // );
-  // const session = await getSession();
+  const todos = await Todos();
+  const allTodos = todos || [];
+  const activeTodos = allTodos.filter(
+    (todo: TodoList) => todo.status !== TodoStatus.COMPLETED
+  );
+  const session = await getSession();
 
   return (
-    <>
-      {/* <TodoListContent todos={activeTodos} session={session} /> */}
-      Hello
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <TodoListContent todos={activeTodos} session={session} />
+    </Suspense>
   );
 };
 
