a1119defe4a3f8ea1d5be49322b7837a64032470
Kens04 <kento090403@icloud.com>
2024-12-28 23:50:28 +0900

    ページコメントアウト

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,18 @@
-import TopDashboardContent from "@/components/dashboard/top-dashboard-content";
-import { getSession } from "@/components/hooks/useSession";
-import { Todos } from "@/components/todo/action";
-import { type TodoList } from "@/components/todo/type";
+// import TopDashboardContent from "@/components/dashboard/top-dashboard-content";
+// import { getSession } from "@/components/hooks/useSession";
+// import { Todos } from "@/components/todo/action";
+// import { type TodoList } from "@/components/todo/type";
 import { Suspense } from "react";
 
 export default async function Dashboard() {
-  const todos: TodoList[] = await Todos();
-  const allTodos = todos || [];
-  const session = await getSession();
+  // const todos: TodoList[] = await Todos();
+  // const allTodos = todos || [];
+  // const session = await getSession();
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <TopDashboardContent todos={allTodos} session={session} />
+      {/* <TopDashboardContent todos={allTodos} session={session} /> */}
+      Hello
     </Suspense>
   );
 }
--- src/app/dashboard/todo-list/page.tsx
+++ src/app/dashboard/todo-list/page.tsx
@@ -1,17 +1,22 @@
-import { getSession } from "@/components/hooks/useSession";
-import { Todos } from "@/components/todo/action";
-import TodoListContent from "@/components/todo/todo-list-content";
-import { TodoStatus, type TodoList } from "@/components/todo/type";
+// import { getSession } from "@/components/hooks/useSession";
+// import { Todos } from "@/components/todo/action";
+// import TodoListContent from "@/components/todo/todo-list-content";
+// import { TodoStatus, type TodoList } from "@/components/todo/type";
 
 const TodoList = async () => {
-  const todos = await Todos();
-  const allTodos = todos || [];
-  const activeTodos = allTodos.filter(
-    (todo: TodoList) => todo.status !== TodoStatus.COMPLETED
-  );
-  const session = await getSession();
+  // const todos = await Todos();
+  // const allTodos = todos || [];
+  // const activeTodos = allTodos.filter(
+  //   (todo: TodoList) => todo.status !== TodoStatus.COMPLETED
+  // );
+  // const session = await getSession();
 
-  return <TodoListContent todos={activeTodos} session={session} />;
+  return (
+    <>
+      {/* <TodoListContent todos={activeTodos} session={session} /> */}
+      Hello
+    </>
+  );
 };
 
 export default TodoList;
