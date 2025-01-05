d5ce8a472e82a9f42f5e2521a5b7afdf96579bf6
Kens04 <68502805+Kens04@users.noreply.github.com>
2025-01-05 12:24:19 +0900

    アイコン変更

--- src/components/layout/user-menu.tsx
+++ src/components/layout/user-menu.tsx
@@ -2,7 +2,7 @@
 
 import { LogoutButton } from "@/components/form/logout-button";
 import { Menu, rem } from "@mantine/core";
-import { IconDashboard } from "@tabler/icons-react";
+import { IconUserCircle } from "@tabler/icons-react";
 import Link from "next/link";
 
 interface UserMenuProps {
@@ -16,7 +16,7 @@ export const UserMenu = ({ children }: UserMenuProps) => {
       <Menu.Dropdown>
         <Menu.Item
           leftSection={
-            <IconDashboard style={{ width: rem(14), height: rem(14) }} />
+            <IconUserCircle style={{ width: rem(14), height: rem(14) }} />
           }
         >
           <Link href="/dashboard/profile">プロフィール</Link>
