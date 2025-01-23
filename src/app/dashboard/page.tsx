aba537ef227ffac5b1ede7315114c0eed4f75261
Kens04 <kento090403@icloud.com>
2025-01-23 22:55:46 +0900

    不要なコード削除

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,16 +2,11 @@ import TopDashboardContent from "@/components/dashboard/top-dashboard-content";
 import { getSession } from "@/components/hooks/useSession";
 import { type TodoList } from "@/components/todo/type";
 import { prisma } from "@/lib/db";
-import { Suspense } from "react";
 
 export default async function Dashboard() {
   const todos = await prisma.todo.findMany();
   const allTodos = todos as TodoList[];
   const session = await getSession();
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <TopDashboardContent todos={allTodos} session={session} />
-    </Suspense>
-  );
+  return <TopDashboardContent todos={allTodos} session={session} />;
 }
